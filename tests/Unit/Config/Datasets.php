f71e88a481335d91bb3f94af7ca814da918dd29d
Wendell Adriel <wendelladriel.ti@gmail.com>
2025-08-10 18:22:11 +0100

    Remove not needed datasets

--- tests/Unit/Config/Datasets.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-dataset('paths', [
-    ["assets/myfile.png", ["assets/", "myfile.png"]],
-    ["./assets/myfile.png", ["./", "assets/", "myfile.png"]],
-    ["./assets/myfile.png", ["./", "assets/", "/myfile.png"]],
-    ["./assets/myfile.png", [".", "assets", "myfile.png"]],
-    ["./export/some.epub", ["./", "export", "some.epub"]],
-]);
