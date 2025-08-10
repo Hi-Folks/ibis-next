6323a1c493e8564973485fdaa0fc65782bbb040b
Wendell Adriel <wendelladriel.ti@gmail.com>
2025-08-10 18:21:37 +0100

    Remove not needed test case

--- tests/Unit/ConfigTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-test('building path dataset', function ($path, $pathToTest) {
-    expect(
-        \Ibis\Config::buildPath(
-            ...$pathToTest,
-        ),
-    )->toBeEqualString($path);
-})->with("paths");
