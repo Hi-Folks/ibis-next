4d89576a725c8692ee58f5ca5a858121f25430a5
Wendell Adriel <wendelladriel.ti@gmail.com>
2025-08-14 15:45:04 +0100

    Update sort content command to Laravel Prompts

--- src/Commands/SortContentCommand.php
+++ src/Commands/SortContentCommand.php
@@ -2,50 +2,37 @@
 
 namespace Ibis\Commands;
 
-use Ibis\Exceptions\InvalidConfigFileException;
-use Ibis\Ibis;
+use Ibis\Concerns\HasConfig;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Support\Str;
-use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use function Laravel\Prompts\info;
 
 class SortContentCommand extends Command
 {
-    private ?Filesystem $disk = null;
+    use HasConfig;
 
-    /**
-     * Configure the command.
-     *
-     * @return void
-     */
-    protected function configure()
+    protected function configure(): void
     {
         $this->setName('content:sort')
             ->setDescription('Sort the files in the content directory.');
     }
 
     /**
-     * Execute the command.
-     *
      * @throws FileNotFoundException
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->disk = new Filesystem();
-        $config = null;
-        try {
-            $config = Ibis::loadConfig();
-        } catch (InvalidConfigFileException $exception) {
-            $output->writeln("<error>{$exception->getMessage()}</error>");
-            $output->writeln('<info>Did you run `ibis-next init`?</info>');
-
-            return Command::FAILURE;
+        if (!$this->init('Sort Content')) {
+            return Command::INVALID;
         }
 
-        collect($this->disk->files($config->getContentPath()))
-            ->each(function ($file, $index) use ($config) {
+        info('✨ Sorting content...');
+
+        collect($this->disk->files($this->config->getContentPath()))
+            ->each(function ($file, $index) {
                 $markdown = $this->disk->get($file->getPathname());
 
                 $newName = Str::slug(sprintf(
@@ -54,8 +41,10 @@ class SortContentCommand extends Command
                     str_replace(['#', '##', '###'], '', explode("\n", $markdown)[0]),
                 ));
 
-                $this->disk->move($file->getPathName(), "{$config->getContentPath()}/{$newName}.md");
+                $this->disk->move($file->getPathName(), "{$this->config->getContentPath()}/{$newName}.md");
             });
+
+        info('✅ Done!');
 
         return Command::SUCCESS;
     }
